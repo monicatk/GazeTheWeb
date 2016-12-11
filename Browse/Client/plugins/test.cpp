dfb226d868844d312995414b75fa1cf761084d96
raphaelmenges <unrealer2@yahoo.de>
2016-12-11 15:23:12 +0100

    Start of plugin system for eyetracker integration.

---
+++ Browse/Client/plugins/test.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+#define DLL_EXPORT __declspec(dllexport) // only on Windows
+
+// Export C interface (resolved overloading etc)
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DLL_EXPORT int __cdecl HelloWorld()
+{
+	return 42;
+}
+
+#ifdef __cplusplus
+}
+#endif
