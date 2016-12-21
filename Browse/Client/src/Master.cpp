ebe146aaead8b1ba1ba5dc043c4e9d4252d3c5fe
raphaelmenges <unrealer2@yahoo.de>
2016-12-21 10:18:12 +0100

    Upgrade GLFW and fix of icon loading.

--- Browse/Client/src/Master.cpp
+++ Browse/Client/src/Master.cpp
@@ -172,6 +172,9 @@ Master::Master(CefMediator* pCefMediator, std::string userDirectory)
 
 	// ### WINDOW ICON ###
 
+	// Set content path (before using it in the helper)
+	eyegui::setRootFilepath(CONTENT_PATH);
+
 	// Load window icons for GLFW
 	std::vector<unsigned char> icon16Data;
 	int icon16Width, icon16Height, icon16ChannelCount;
@@ -205,9 +208,6 @@ Master::Master(CefMediator* pCefMediator, std::string userDirectory)
 		icons.data());
 
     // ### EYEGUI ###
-
-    // Set content path
-    eyegui::setRootFilepath(CONTENT_PATH);
 
     // Set print callbacks
     std::function<void(std::string)> printGUICallback = [&](std::string message) { this->GUIPrintCallback(message); };
