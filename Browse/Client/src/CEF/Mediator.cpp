97c94e1268d51b3292e69c990729a8574eda0073
Daniel Müller <muellerd@uni-koblenz.de>
2017-01-09 20:49:22 +0100

    ... removed test logging input. :D

--- Browse/Client/src/CEF/Mediator.cpp
+++ Browse/Client/src/CEF/Mediator.cpp
@@ -496,10 +496,6 @@ std::weak_ptr<DOMNode> Mediator::GetDOMNode(CefRefPtr<CefBrowser> browser, DOMNo
 
 void Mediator::ShowDevTools()
 {
-
-	// DEBUG/TEST
-	_handler->LogGlobalEventInBrowserContext("YOLO");
-
 	LogInfo("CefMediator: Showing DevTools...");
 
 	// Setup
