0f9e1b2aa5f747f509b8a820583447491462397c
Daniel Müller <muellerd@uni-koblenz.de>
2017-03-24 10:03:59 +0100

    Introduced extraction namespaces for easily keeping track of extraction definitions

---
+++ Browse/Client/src/CEF/Data/DOMExtraction.cpp
@@ -0,0 +1,165 @@
+//============================================================================
+// Distributed under the Apache License, Version 2.0.
+// Author: Daniel Mueller (muellerd@uni-koblenz.de)
+// Author: Raphael Menges (raphaelmenges@uni-koblenz.de)
+//============================================================================
+
+#include "DOMExtraction.h"
+#include <src/Utils/Helper.h>
+
+const CefRefPtr<CefListValue> V8ToCefListValue::NestedListOfDoubles(CefRefPtr<CefV8Value> v8rects)
+{
+	if (!v8rects->IsArray())
+		return CefRefPtr<CefListValue>();
+
+	auto rects = CefListValue::Create();
+	for (int i = 0; i < v8rects->GetArrayLength(); i++)
+	{
+		CefRefPtr<CefV8Value> v8rect = v8rects->GetValue(i);
+
+		if (!v8rect->IsArray())
+			return CefRefPtr<CefListValue>();
+
+		CefRefPtr<CefListValue> rect = CefListValue::Create();
+		for (int j = 0; j < v8rect->GetArrayLength(); j++)
+		{
+			rect->SetDouble(j, v8rect->GetValue(j)->GetDoubleValue());
+		}
+		rects->SetList(i, rect);
+	}
+	return rects;
+}
+
+const CefRefPtr<CefListValue> V8ToCefListValue::ListOfStrings(CefRefPtr<CefV8Value> attrData)
+{
+	if (!attrData->IsArray())
+		return CefRefPtr<CefListValue>();
+
+	CefRefPtr<CefListValue> list = CefListValue::Create();
+	for (int i = 0; i < attrData->GetArrayLength(); i++)
+	{
+		CefRefPtr<CefValue> val = CefValue::Create();
+
+		if (!attrData->GetValue(i)->IsString())
+			continue;
+
+		val->SetString(attrData->GetValue(i)->GetStringValue());
+		list->SetValue(i, val);
+	}
+
+	return list;
+}
+
+const CefRefPtr<CefListValue> V8ToCefListValue::Boolean(CefRefPtr<CefV8Value> attrData)
+{
+	if (!attrData->IsBool())
+		return CefRefPtr<CefListValue>();
+
+	CefRefPtr<CefListValue> list = CefListValue::Create();
+	CefRefPtr<CefValue> val = CefValue::Create();
+	val->SetInt(attrData->GetBoolValue());
+	list->SetValue(0, val);
+	return list;
+}
+
+const CefRefPtr<CefListValue> V8ToCefListValue::Integer(CefRefPtr<CefV8Value> attrData)
+{
+	if (!attrData->IsInt())
+		return CefRefPtr<CefListValue>();
+
+	CefRefPtr<CefListValue> list = CefListValue::Create();
+	CefRefPtr<CefValue> id = CefValue::Create();
+	id->SetInt(attrData->GetIntValue());
+	list->SetValue(0, id);
+	return list;
+}
+
+const CefRefPtr<CefListValue> V8ToCefListValue::String(CefRefPtr<CefV8Value> attrData)
+{
+	if (!attrData->IsString())
+		return CefRefPtr<CefListValue>();
+
+	CefRefPtr<CefListValue> list = CefListValue::Create();
+	CefRefPtr<CefValue> val = CefValue::Create();
+	val->SetInt(attrData->GetIntValue());
+	list->SetValue(0, val);
+	return list;
+}
+
+const CefRefPtr<CefListValue> StringToCefListValue::NestedListOfDoubles(std::string attrData)
+{
+	CefRefPtr<CefListValue> extracted_data = CefValue::Create();
+	std::vector<std::string> rectDataStr = SplitBySeparator(attrData, ';');
+	std::vector<double> rectData;
+
+	// Extract each float value from string, earlier separated by ';', and convert string to numerical value
+	std::for_each(
+		rectDataStr.begin(),
+		rectDataStr.end(),
+		[&rectData](std::string value) {rectData.push_back(std::stod(value)); }
+	);
+
+	// Read out each 4 float values und create 1 Rect with them
+	for (int i = 0; i + 3 < rectData.size(); i += 4)
+	{
+		CefRefPtr<CefListValue> rect = CefListValue::Create();
+		for (int j = 0; j < 4; j++)
+		{
+			CefRefPtr<CefValue> val = CefValue::Create();
+			val->SetDouble(rectData[i + j]);
+			rect->SetValue(j, val);
+		}
+
+		extracted_data->SetValue(i / 4, rect);
+	}
+
+	return extracted_data;
+}
+
+const CefRefPtr<CefListValue> StringToCefListValue::ListOfStrings(std::string attrData)
+{
+	CefRefPtr<CefListValue> extracted_data = CefValue::Create();
+
+	std::vector<std::string> options = SplitBySeparator(attrData, ';');
+	for (int i = 0; i < options.size(); i++)
+	{
+		CefRefPtr<CefValue> option = CefValue::Create();
+		option->SetString(options[i]);
+		extracted_data->SetValue(i, option);
+	}
+
+	return extracted_data;
+}
+
+const CefRefPtr<CefListValue> StringToCefListValue::Boolean(std::string attrData)
+{
+	CefRefPtr<CefListValue> extracted_data = CefValue::Create();
+
+	CefRefPtr<CefValue> val = CefValue::Create();
+	val->SetBool(std::stoi(attrData));
+	extracted_data->SetValue(0, val);
+
+	return extracted_data;
+}
+
+const CefRefPtr<CefListValue> StringToCefListValue::Integer(std::string attrData)
+{
+	CefRefPtr<CefListValue> extracted_data = CefValue::Create();
+
+	CefRefPtr<CefValue> val = CefValue::Create();
+	val->SetInt(std::stoi(attrData));
+	extracted_data->SetValue(0, val);
+
+	return extracted_data;
+}
+
+const CefRefPtr<CefListValue> StringToCefListValue::String(std::string attrData)
+{
+	CefRefPtr<CefListValue> extracted_data = CefValue::Create();
+
+	CefRefPtr<CefValue> val = CefValue::Create();
+	val->SetString(attrData);
+	extracted_data->SetValue(0, val);
+
+	return extracted_data;
+}
--- Browse/Client/src/CEF/Data/DOMExtraction.h
+++ Browse/Client/src/CEF/Data/DOMExtraction.h
@@ -7,154 +7,118 @@
 #ifndef DOMEXTRACTION_H_
 #define DOMEXTRACTION_H_
 
+#include <src/Utils/Logger.h>
 #include <include/cef_values.h>
 #include <include/cef_v8.h>
 #include <map>
 #include <functional>
+#include <algorithm>
 
 enum DOMAttribute {
 	Rects = 0, FixedId, OverflowId,
 	Text, IsPassword, Url, Options
 };
 
-const std::map<const DOMAttribute, const std::string> attrGetter = {
-	{DOMAttribute::Rects,		"getRects"},
-	{DOMAttribute::FixedId,		"getFixedId"},
-	{DOMAttribute::OverflowId,	"getOverflowId"},
-	{DOMAttribute::Text,		"getText"},
-	{DOMAttribute::IsPassword,	"getPassword"},
-	{DOMAttribute::Url,			"getUrl"},
-	{DOMAttribute::Options,		"getOptions"}
-
-};
-
-CefRefPtr<CefListValue> extractNestedListOfDoubles(CefRefPtr<CefV8Value> v8rects)
+/** Do only use if V8 context was entered before! */
+namespace V8ToCefListValue
 {
-	if (!v8rects->IsArray())
-		return CefRefPtr<CefListValue>();
+	// Nested lists
+	const static CefRefPtr<CefListValue> NestedListOfDoubles(CefRefPtr<CefV8Value> v8rects);
 
-	auto rects = CefListValue::Create();
-	for (int i = 0; i < v8rects->GetArrayLength(); i++)
+	// Lists
+	const static CefRefPtr<CefListValue> ListOfStrings(CefRefPtr<CefV8Value> attrData);
+
+	// Primitive types - TODO: There certainly exists a more generic approach for each primitive type!
+	const static CefRefPtr<CefListValue> Boolean(CefRefPtr<CefV8Value> attrData);
+	const static CefRefPtr<CefListValue> Integer(CefRefPtr<CefV8Value> attrData);
+	const static CefRefPtr<CefListValue> String(CefRefPtr<CefV8Value> attrData);
+
+	const std::map<const DOMAttribute, const std::string> AttrGetter = {
+		{ DOMAttribute::Rects,		"getRects" },
+		{ DOMAttribute::FixedId,	"getFixedId" },
+		{ DOMAttribute::OverflowId,	"getOverflowId" },
+		{ DOMAttribute::Text,		"getText" },
+		{ DOMAttribute::IsPassword,	"getPassword" },
+		{ DOMAttribute::Url,		"getUrl" },
+		{ DOMAttribute::Options,	"getOptions" }
+
+	};
+
+	const std::map < const DOMAttribute, const std::function<CefRefPtr<CefListValue>(CefRefPtr<CefV8Value>)> > AttrConversion = {
+		{ DOMAttribute::Rects,		&NestedListOfDoubles },
+		{ DOMAttribute::FixedId,	&Integer },
+		{ DOMAttribute::OverflowId,	&Integer },
+		{ DOMAttribute::Text,		&String },
+		{ DOMAttribute::IsPassword,	&Boolean },
+		{ DOMAttribute::Url,		&String },
+		{ DOMAttribute::Options,	&ListOfStrings }
+	
+	};
+
+	const static CefRefPtr<CefListValue> ExtractAttributeData(DOMAttribute attr, CefRefPtr<CefV8Value> obj)
 	{
-		CefRefPtr<CefV8Value> v8rect = v8rects->GetValue(i);
+		// Early exit
+		if (AttrGetter.find(attr) == AttrGetter.end() || AttrConversion.find(attr) == AttrConversion.end())
+		{
+			LogError("V8ToCefListValue conversion: Insufficient information defined for DOMAttribute ", (int)attr, "! Abort.");
+			return CefV8Value::CreateNull();
+		}
 
-		if (!v8rect->IsArray())
+		// Return getter function,
+		CefRefPtr<CefV8Value> getter = obj->GetValue(AttrGetter.at(attr));
+
+		// Check if getter function is valid
+		if (!getter->IsFunction())
+		{
+			LogBug("V8ToCefListValue conversion: Could not access getter function for DOMAttribute ", (int)attr, " in Javascript object.");
+			return CefV8Value::CreateNull();
+		}
+
+		// Execute getter function
+		CefRefPtr<CefV8Value> data = getter->ExecuteFunction(obj, {});
+
+		// Convert returned V8Value to CefListValue
+		return AttrConversion.at(attr)(data);
+	}
+}
+
+
+namespace StringToCefListValue
+{
+	// Nested lists
+	const static CefRefPtr<CefListValue> NestedListOfDoubles(std::string attrData);
+
+	// Lists
+	const static CefRefPtr<CefListValue> ListOfStrings(std::string attrData);
+
+	// Primitive types
+	const static CefRefPtr<CefListValue> Boolean(std::string attrData);
+	const static CefRefPtr<CefListValue> Integer(std::string attrData);
+	const static CefRefPtr<CefListValue> String(std::string attrData);
+
+	const std::map<const DOMAttribute, const std::function<CefRefPtr<CefListValue>(std::string)> > AttrConversion =
+	{
+		{DOMAttribute::Rects,		&NestedListOfDoubles},
+		{DOMAttribute::FixedId,		&Integer},
+		{DOMAttribute::OverflowId,	&Integer},
+		{DOMAttribute::Text,		&String},
+		{DOMAttribute::IsPassword,	&Boolean},
+		{DOMAttribute::Url,			&String},
+		{DOMAttribute::Options,		&ListOfStrings}
+	};
+
+
+	const static CefRefPtr<CefListValue> ExtractAttributeData(DOMAttribute attr, std::string attrData)
+	{
+		// Early exit
+		if (AttrConversion.find(attr) == AttrConversion.end())
+		{
+			LogError("StringToCefListValue conversion: Insufficient information defined for DOMAttribute ", (int)attr, "! Abort.");
 			return CefRefPtr<CefListValue>();
-
-		CefRefPtr<CefListValue> rect = CefListValue::Create();
-		for (int j = 0; j < v8rect->GetArrayLength(); j++)
-		{
-			rect->SetDouble(j, v8rect->GetValue(j)->GetDoubleValue());
 		}
-		rects->SetList(i, rect);
+
+		return AttrConversion.at(attr)(attrData);
 	}
-	return rects;
-
-}
-
-const std::map < const DOMAttribute, const std::function<CefRefPtr<CefListValue>(CefRefPtr<CefV8Value>)> > v8ToListValue = {
-	// RECTS
-	{DOMAttribute::Rects, &extractNestedListOfDoubles},
-	// FIXEDID
-	{DOMAttribute::FixedId, [](const CefRefPtr<CefV8Value> v8id) {
-			if (!v8id->IsInt())
-				return CefRefPtr<CefListValue>();
-
-			CefRefPtr<CefListValue> list = CefListValue::Create();
-			auto id = CefValue::Create();
-			id->SetInt(v8id->GetIntValue());
-			list->SetValue(0, id);
-			return list;
-		}
-	}
-};
-
-
-const static CefRefPtr<CefListValue> ExtractAttributeDataFromString(DOMAttribute attr, std::string attrData)
-{
-	CefRefPtr<CefListValue> extracted_data = CefValue::Create();
-
-	switch (attr)
-	{
-	case(DOMAttribute::Rects):
-	{
-		std::vector<std::string> rectDataStr = SplitBySeparator(attrData, ';');
-		std::vector<double> rectData;
-
-		// Extract each float value from string, earlier separated by ';', and convert string to numerical value
-		std::for_each(
-			rectDataStr.begin(),
-			rectDataStr.end(),
-			[&rectData](std::string value) {rectData.push_back(std::stod(value)); }
-		);
-
-		// Read out each 4 float values und create 1 Rect with them
-		for (int i = 0; i + 3 < rectData.size(); i += 4)
-		{
-			CefRefPtr<CefListValue> rect = CefListValue::Create();
-			for (int j = 0; j < 4; j++)
-			{
-				CefRefPtr<CefValue> val = CefValue::Create();
-				val->SetDouble(rectData[i + j]);
-				rect->SetValue(j, val);
-			}
-
-			extracted_data->SetValue(i / 4, rect);
-		}
-		break;
-	};
-	case(DOMAttribute::FixedId):
-	case(DOMAttribute::OverflowId):
-	{
-		CefRefPtr<CefValue> val = CefValue::Create();
-		val->SetInt(std::stoi(attrData));
-		extracted_data->SetValue(0, val);
-		break;
-	};
-	case(DOMAttribute::Text):
-	case(DOMAttribute::Url):
-	{
-		CefRefPtr<CefValue> val = CefValue::Create();
-		val->SetString(attrData);
-		extracted_data->SetValue(0, val);
-		break;
-	}
-	case(DOMAttribute::IsPassword):
-	{
-		CefRefPtr<CefValue> val = CefValue::Create();
-		val->SetBool(std::stoi(attrData));
-		extracted_data->SetValue(0, val);
-		break;
-	}
-	case(DOMAttribute::Options):
-	{
-		std::vector<std::string> options = SplitBySeparator(attrData, ';');
-		for (int i = 0; i < options.size(); i++)
-		{
-			CefRefPtr<CefValue> option = CefValue::Create();
-			option->SetString(options[i]);
-			extracted_data->SetValue(i, option);
-		}
-		break;
-	}
-	}
-	return extracted_data;
-}
-
-const static CefRefPtr<CefListValue> ExtractAttributeDataFromV8Object(DOMAttribute attr, CefRefPtr<CefV8Value> obj)
-{
-	// Return getter function, if known and listed in map, otherwise return NULL
-	CefRefPtr<CefV8Value> getter = (attrGetter.find(attr) != attrGetter.end()) ? 
-		obj->GetValue(attrGetter.at(attr)) : CefV8Value::CreateNull();	
-
-	// Check if getter function is valid
-	if (!getter->IsFunction())
-		return CefV8Value::CreateNull();
-
-	// Execute getter function
-	CefRefPtr<CefV8Value> data = getter->ExecuteFunction(obj, {});
-
-	// Convert returned V8Value to CefListValue
 }
 
 #endif // DOMEXTRACTION_H_
