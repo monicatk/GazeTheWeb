94e1336c19cec8039253e8c750395df688f2cb17
Daniel Müller <muellerd@uni-koblenz.de>
2017-07-14 15:41:45 +0200

    Print performance stats with bitmask creation time

--- Browse/Client/content/javascript/dom_nodes.js
+++ Browse/Client/content/javascript/dom_nodes.js
@@ -124,19 +124,21 @@ DOMNode.prototype.updateRects = function(altNode){
     {
         this.rects = adjustedRects;
 
-        // if(this.rects.length > 0)
-        // {
-        //     // TODO: Only update rects, if position contained in window?
-        //     // DEBUG - toplayer bitmap
-        //     var bm = [];
-        //     var r = this.rects[0];
-        //     bm.push(document.elementFromPoint(r[0],r[1]));
-        //     bm.push(document.elementFromPoint(r[0],r[3]));
-        //     bm.push(document.elementFromPoint(r[2],r[3]));
-        //     bm.push(document.elementFromPoint(r[2],r[1]));
-        //     this.bitmap = bm;
-        //     ConsolePrint("#TEST#"+bm+"#");
-        // }
+        var t1 = performance.now();
+        if(this.rects.length > 0)
+        {
+            // TODO: Only update rects, if position contained in window?
+            // DEBUG - toplayer bitmap
+            var bm = [];
+            var r = this.rects[0];
+            bm.push(document.elementFromPoint(r[0],r[1]));
+            bm.push(document.elementFromPoint(r[0],r[3]));
+            bm.push(document.elementFromPoint(r[2],r[3]));
+            bm.push(document.elementFromPoint(r[2],r[1]));
+            this.bitmap = bm;
+            ConsolePrint("#TEST#"+bm+"#");
+        }
+        UpdateBitmaskTimer(t1);
 
         SendAttributeChangesToCEF("Rects", this);
         UpdateRectUpdateTimer(t0);
