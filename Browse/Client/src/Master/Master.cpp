0529bba60966e1c7865219cfd2221a84a95c6540
Raphael Menges <unrealer2@yahoo.de>
2017-10-04 17:02:08 +0200

    Windows tile design.

--- Browse/Client/src/Master/Master.cpp
+++ Browse/Client/src/Master/Master.cpp
@@ -334,9 +334,9 @@ Master::Master(Mediator* pCefMediator, std::string userDirectory)
 
     // ### HOMEPAGE ###
 	// _upWeb->AddTab("https://developer.mozilla.org/en-US/docs/Web/CSS/overflow");
-	_upWeb->AddTab("http://html5-demos.appspot.com/static/fullscreen.html");
+	// _upWeb->AddTab("http://html5-demos.appspot.com/static/fullscreen.html");
 	// _upWeb->AddTab(std::string(CONTENT_PATH) + "/websites/index.html");
-	// _upWeb->AddTab(_upSettings->GetHomepage());
+	_upWeb->AddTab(_upSettings->GetHomepage());
 
     // ### SUPER LAYOUT ###
 
