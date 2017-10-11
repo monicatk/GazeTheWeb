96163730026cf99c152383682dca04ee01993981
Raphael Menges <unrealer2@yahoo.de>
2017-10-11 15:32:38 +0200

    Setup.h

--- Browse/Client/src/Setup.h
+++ Browse/Client/src/Setup.h
@@ -38,9 +38,9 @@ namespace setup
 
 	// Control TODO: move connect bools to config file
 	static const bool	CONNECT_OPEN_GAZE = false;
-	static const bool	CONNECT_SMI_IVIEWX = true;
-	static const bool	CONNECT_VI_MYGAZE = false | DEPLOYMENT;
-	static const bool	CONNECT_TOBII_EYEX = true;
+	static const bool	CONNECT_SMI_IVIEWX = false;
+	static const bool	CONNECT_VI_MYGAZE = true | DEPLOYMENT;
+	static const bool	CONNECT_TOBII_EYEX = false;
 	static const float	DURATION_BEFORE_INPUT = 1.f; // wait one second before accepting input
 	static const float	MAX_AGE_OF_USED_GAZE = 1.f; // only accept gaze as input that is not older than one second (TODO: this is not used by filter but by master to determine when to stop taking gaze input as serious)
 	static const float	DURATION_BEFORE_SUPER_CALIBRATION = 3.f; // duration until recalibration is offered after receiving no gaze samples
